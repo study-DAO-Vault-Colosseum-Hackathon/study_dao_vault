a31df21d2a630305709707399878bf6413a4791d
bijesh-09 <bijeshkaranjit@gmail.com>
2026-05-08 07:40:45 +0000

    exports verifyToken

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,20 +1,4 @@
-const { auth } = require("../utils/firebase");
+const { authenticateMagic } = require('./magic-auth');
 
-const verifyToken = async (req, res, next) => {
-  const idToken = req.headers.authorization?.split('Bearer ')[1];
-
-  if (!idToken) {
-    return res.status(401).json({ error: "Unauthorized: No token provided" });
-  }
-
-  try {
-    const decodedToken = await auth.verifyIdToken(idToken);
-    req.user = decodedToken;
-    next();
-  } catch (error) {
-    console.error("Token verification failed:", error.message);
-    return res.status(401).json({ error: "Unauthorized: Invalid token" });
-  }
-};
-
-module.exports = { verifyToken };
+// Export Magic authenticator as default auth middleware
+module.exports = { verifyToken: authenticateMagic };
