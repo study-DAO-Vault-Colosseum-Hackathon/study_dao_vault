18cdf7e21d727d26ccb53fc2c0066082733513b1
swastik972 <rawatswastik30@gmail.com>
2026-05-10 16:39:40 +0545

    feat: initialize Vite configuration with path aliases, environment variables, and proxy settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, __dirname, '')
+  const env = loadEnv(mode, process.cwd(), 'VITE_')
 
   return {
     plugins: [react()],
